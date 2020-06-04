9248ddbd9fcf228117727b7a6e8a0d9d3c03787c
FErookie <332154783@qq.com>
2020-06-04 22:30:17 +0800

    add

--- www/index.js
+++ www/index.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const https = require('https');
 
 const options = {
-    key: fs.readFileSync("./ssl/selfsigned.key"),
-    cert: fs.readFileSync("./ssl/selfsigned.crt")
+    key: fs.readFileSync("./ssl/2_www.ferookie.club.key"),
+    cert: fs.readFileSync("./ssl/1_www.ferookie.club_bundle.crt")
 }
 https.createServer(options, app.callback()).listen(3000, ()=>{
     console.log("成功启动https");
